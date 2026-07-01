9c34b002b21d53c20b331f14990b5f1ae92c96ae
fud00 <kennethjrdean99@gmail.com>
2026-07-01 23:37:44 +0800

    fix: add comment to clarify API route purpose in route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -5,6 +5,7 @@ import {
   syncUserDeletion,
   syncUserUpdation,
 } from "@/inngest/functions";
+// This is the API route that Inngest will call when events are triggered
 
 export const { GET, POST, PUT } = serve({
   client: inngest,

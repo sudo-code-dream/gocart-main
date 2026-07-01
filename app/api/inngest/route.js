d54f05d53731eca7199f40ab0c8446bdf63c4195
fud00 <kennethjrdean99@gmail.com>
2026-07-01 23:22:41 +0800

    fix formatting and correct typo in route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,12 @@
-import {serve } from "inngest/next";
+import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
-import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/inngest/functions";
+import {
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+} from "@/inngest/functions";
 
-export const { GET, POST, PUT} = serve({
-    clint: inngest,
-    functions: [
-        syncUserCreation,
-        syncUserUpdation,
-        syncUserDeletion
-    ]
-})
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [syncUserCreation, syncUserUpdation, syncUserDeletion],
+});

bef5eaa996d7d33e8038a064be7a531e0a30a8e1
fud00 <kennethjrdean99@gmail.com>
2026-07-18 19:31:37 +0800

    feat: improve layout and styling of shop and best selling components

--- app/(public)/shop/page.jsx
+++ app/(public)/shop/page.jsx
@@ -21,15 +21,23 @@ import { useSelector } from "react-redux"
         : products;
 
     return (
-        <div className="min-h-[70vh] mx-6">
-            <div className=" max-w-7xl mx-auto">
-                <h1 onClick={() => router.push('/shop')} className="text-2xl text-slate-500 my-6 flex items-center gap-2 cursor-pointer"> {search && <MoveLeftIcon size={20} />}  All <span className="text-slate-700 font-medium">Products</span></h1>
-                <div className="grid grid-cols-2 sm:flex flex-wrap gap-6 xl:gap-12 mx-auto mb-32">
-                    {filteredProducts.map((product) => <ProductCard key={product.id} product={product} />)}
-                </div>
-            </div>
+      <div className='min-h-[70vh] mx-6'>
+        <div className=' mx-auto max-w-7xl px-6 my-20'>
+          <h1
+            onClick={() => router.push("/shop")}
+            className='text-2xl text-slate-500 my-6 flex items-center gap-2 cursor-pointer'>
+            {" "}
+            {search && <MoveLeftIcon size={20} />} All{" "}
+            <span className='text-slate-700 font-medium'>Products</span>
+          </h1>
+          <div className='mt-10 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {filteredProducts.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 
